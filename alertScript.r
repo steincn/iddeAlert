95820ef503bd535ef17d84b8c7534a692d2683e5
steincn <30835262+steincn@users.noreply.github.com>
2017-12-15 11:40:44 -0500

    Delete Precip & hourly
    Not needed for this project

--- alertScript.r
+++ alertScript.r
@@ -21,36 +21,16 @@ recentQ <- readNWISuv(siteNumbers="02146409", # litte sugar
                              parameterCd=QParameterCd,
                              startDate=startTime, tz="America/New_York")
 
-## Subset out last hours worth
-rQ <- filter(recentQ, dateTime>Sys.time()-3600)
 
-#############################################################################
-## Precip
-PParameterCd <- "00045" ## rainfall inches
 
-## Pull most recent data
-recentP <- readNWISuv(siteNumbers="351229080480145", ## CRN 66
-                             parameterCd=PParameterCd,
-                             startDate=startTime, tz="America/New_York")
 
-## Subset out last TWO hours worth
-rP <- filter(recentP, dateTime>Sys.time()-7200)
-
-## Pull most recent data
-recentP2 <- readNWISuv(siteNumbers="351320080502645", ## CRN 15
-                             parameterCd=PParameterCd,
-                             startDate=startTime, tz="America/New_York")
-
-## Subset out last TWO hours worth
-rP2 <- filter(recentP2, dateTime>Sys.time()-7200)
 
 #############################################################################
 ## Print to log file
 print("########")
 print(Sys.time())
 print(rQ[,4])
-print(rP[,4])
-print(rP2[,4])
+
 
 
 #############################################################################
